662abcb7c06865320ce2d97a6fb1319bbb652139
AngelaQYang <sdu.yangqiqian@gmail.com>
2018-12-11 14:03:45 -0800

    add BRT buffer area (1/4 miles) all ferry, rails are 1/2 miles

--- item8_proximity_to_HCT_fix_parcel_bug.py
+++ item8_proximity_to_HCT_fix_parcel_bug.py
@@ -3,6 +3,14 @@ import numpy as np
 import os 
 import datetime
 output_path = 'U:\\angela\\job_housing\\soundcast_2050\\job_housing_commute\\vision_long_list'
+tract_id_to_census_tract_file = 'U:\\angela\\job_housing\\soundcast_2050\\inputs\\accessibility\\tract_id_to_census_tract.csv'
+parcel_tract_id_file = 'U:\\angela\\job_housing\\soundcast_2050\\inputs\\accessibility\\parcel_geographies_to_tract_id.csv'
+
+ #buffer_file = 'U:\\angela\\job_housing\\soundcast_2050\\job_housing_commute\\vision_long_list\\input\\2017HTC_parcels.txt'
+ ## buffer for both HTC and BRT
+buffer_file = 'T:\\2018December\\Angela\\2018Network\\ALL_STOPS_2017_parcel_clip.csv'
+
+
 
 ## 1. urbansim parcel to population file (2014 data)
 model_path = r'L:\vision2050\soundcast\integrated\final_runs\base_year\2014'
@@ -10,10 +18,8 @@ parcel_file_name = 'inputs\scenario\landuse\parcels_urbansim.txt'
 parcel_urbansim = pd.read_csv(os.path.join(model_path, parcel_file_name), sep = ' ')
 
 ## 2. parcel to census look up tables
-parcel_tract_id = 'U:\\angela\\job_housing\\soundcast_2050\\inputs\\accessibility\\parcel_geographies_to_tract_id.csv'
-parcel_tract_id = pd.read_csv(parcel_tract_id)
-tract_id_to_census_tract = 'U:\\angela\\job_housing\\soundcast_2050\\inputs\\accessibility\\tract_id_to_census_tract.csv'
-tract_id_to_census_tract = pd.read_csv(tract_id_to_census_tract)
+parcel_tract_id = pd.read_csv(parcel_tract_id_file)
+tract_id_to_census_tract = pd.read_csv(tract_id_to_census_tract_file)
 my_parcel_census_tract = pd.merge(parcel_tract_id, tract_id_to_census_tract, left_on='census_tract_id', right_on='census_tract_id', how='outer')
 # check if there is any mismatched raw 
 print my_parcel_census_tract.isnull().sum()
@@ -22,35 +28,48 @@ print my_parcel_census_tract.isnull().sum()
 parcel_census_urbansim = pd.merge(parcel_urbansim, my_parcel_census_tract, left_on = 'PARCELID', right_on = 'parcel_id')
 # check if there is any mismatch 
 print parcel_census_urbansim.isnull().sum()
+'''
+attention: if it is HTC, then the HH_P suppose to be HH_P_x and HH_P_y. Same as all HH_P below. they could be HH_P_x. 
+There is no big different, either x or y works. 
+'''
 parcel_census_urbansim = parcel_census_urbansim[['parcel_id', 'geoid10', 'HH_P']]
+# GET THE TOTAL HH POPULATION
+parcel_census_urbansim_HH_P = parcel_census_urbansim.groupby(['geoid10'])[['HH_P']].sum()
+parcel_census_urbansim_HH_P.reset_index(inplace=True)
+hh_p_dict = parcel_census_urbansim_HH_P.set_index(['geoid10']).to_dict()['HH_P']
 
 
 # 4. the second clip of HTC (with household information)
-HCT_buffer = pd.read_csv('U:\\angela\\job_housing\\soundcast_2050\\job_housing_commute\\vision_long_list\\input\\2017HTC_parcels.txt')
+buffer = pd.read_csv(buffer_file)
 # make sure data types are same, so merge would be all matched
 ## wried rounding problem (5.0 will become 4). Solved by using .round(0)astype(int)
-HCT_buffer['PSRC_ID'] = HCT_buffer['PSRC_ID'].round(0).astype(int)
+buffer['PSRC_ID'] = buffer['PSRC_ID'].round(0).astype(int)
 
 ## 5. merge HCT buffer with census tract 
-HCT_buffer_census = pd.merge(HCT_buffer, parcel_census_urbansim, left_on = 'PSRC_ID', right_on = 'parcel_id', how = 'left')
-print HCT_buffer_census.isnull().sum()
+buffer_census = pd.merge(buffer, parcel_census_urbansim, left_on = 'PSRC_ID', right_on = 'parcel_id', how = 'left')
+print buffer_census.isnull().sum()
 
 
-## 6. groupby 
-HCT_buffer_census = HCT_buffer_census[['geoid10', 'parcel_id', 'PSRC_ID', 'HH_P_x', 'HH_P_y']]
-print HCT_buffer_census['HH_P_y'].sum()
-HCT_buffer_census_groupby = HCT_buffer_census.groupby(['geoid10'])[['HH_P_y']].sum()
-HCT_buffer_census_groupby.reset_index(inplace=True)
-print HCT_buffer_census_groupby['HH_P_y'].sum()
+## 6. groupby the buffer area's household population
+buffer_census = buffer_census[['geoid10', 'parcel_id', 'PSRC_ID', 'HH_P']]
+print buffer_census['HH_P'].sum()
+buffer_census_groupby = buffer_census.groupby(['geoid10'])[['HH_P']].sum()
+buffer_census_groupby.reset_index(inplace=True)
+print buffer_census_groupby['HH_P'].sum()
 
 
 ## 7. get all census tract 
 census_df = tract_id_to_census_tract[['geoid10']]
-final = pd.merge(HCT_buffer_census_groupby, census_df, left_on='geoid10', right_on='geoid10', how='right')
+final = pd.merge(buffer_census_groupby, census_df, left_on='geoid10', right_on='geoid10', how='right')
+
+# 8. map the totalhousehold population into the final table
+final['HH_P_TOT'] = final['geoid10'].map(hh_p_dict)
 final = final.fillna(0)
-## 7. write out data
+final['buffer_proportion'] = final['HH_P'] / final['HH_P_TOT']
+
+## 9. write out data
 today = datetime.date.today()
-file_name = 'item8_proximity_to_HTC_' + str(today) + '.csv'
+file_name = 'item8_proximity_to_' + str(today) + '.csv'
 final.to_csv(os.path.join(output_path, file_name), index=False)
 
 
--- item9_proximity_to_HCT_2025_fix_bug.py
+++ item9_proximity_to_HCT_2025_fix_bug.py
@@ -12,8 +12,10 @@ geo_path = 'U:\\angela\job_housing\soundcast_2050\inputs\\accessibility'
 parcel_tract_id = 'parcel_geographies_to_tract_id.csv'
 tract_id_to_census_tract = 'tract_id_to_census_tract.csv'
 ## below two files are clipped from parcel GIS file: J:\Projects\UrbanSim\NEW_DIRECTORY\GIS\Shapefiles\Parcels\Region\2014\gapwork\prcl15_4kpt.shp
-HCT_area_file_name = 'parcel_HCT_area_2025.txt'
 parcel_area_file_name = 'parcel_area.txt'
+
+buffer_area_file_name = 'T:\\2018December\\Angela\\1_2025 Network\\all_2025_STOPS_buffer_parcel.csv'
+
 
 if year == 2014:
     if scenario == 'loads':
@@ -35,7 +37,7 @@ parcel id is: PSRC_ID
 parcel size: Shape_Area (The unit of measure for the shape_area should be square feet.)
 '''
 parcel_area = pd.read_csv(os.path.join(output_path, parcel_area_file_name), sep = ',')
-# fixed the bug
+############# fixed the bug
 parcel_area['PSRC_ID'] = parcel_area['PSRC_ID'].round(0).astype(int)
 
 ## 2. parcel to census look up tables
@@ -45,29 +47,29 @@ my_parcel_census_tract = pd.merge(parcel_tract_id, tract_id_to_census_tract, lef
 
 parcel_area_geo = pd.merge(parcel_area, my_parcel_census_tract, left_on = 'PSRC_ID', right_on = 'parcel_id', how='left')
 
-# HCT area (no water)
+# 3. buffer area (no water)
 '''
 the parcel file was preprocessed in Arc GIS, clipped with HCT buffer area
 '''
-HCT_area = pd.read_csv(os.path.join(output_path, HCT_area_file_name), sep = ',')
-# fixed the bug
-HCT_area['PSRC_ID'] = HCT_area['PSRC_ID'].round(0).astype(int)
-HCT_area['HCT_Area'] = HCT_area['Shape_Area']
-HCT_area = HCT_area[[ 'PSRC_ID', 'HCT_Area']]
+buffer_area = pd.read_csv(buffer_area_file_name)
+############ fixed the bug
+buffer_area['PSRC_ID'] = buffer_area['PSRC_ID'].round(0).astype(int)
+buffer_area['Buffer_Area'] = buffer_area['Shape_Area']
+buffer_area = buffer_area[[ 'PSRC_ID', 'Buffer_Area']]
 
-# areas in HCT parcel area
-all_parcel_HTC = pd.merge(HCT_area, parcel_area_geo, left_on='PSRC_ID', right_on='parcel_id', how='right')
-all_parcel_HTC = all_parcel_HTC[['parcel_id', 'geoid10', 'HCT_Area', 'Shape_Area']]
-all_parcel_HTC = all_parcel_HTC.groupby(['geoid10'])[['HCT_Area', 'Shape_Area']].sum()
+# areas in buffer parcel area
+all_parcel_HTC = pd.merge(buffer_area, parcel_area_geo, left_on='PSRC_ID', right_on='parcel_id', how='right')
+all_parcel_HTC = all_parcel_HTC[['parcel_id', 'geoid10', 'Buffer_Area', 'Shape_Area']]
+all_parcel_HTC = all_parcel_HTC.groupby(['geoid10'])[['Buffer_Area', 'Shape_Area']].sum()
 all_parcel_HTC.reset_index(inplace=True)
 all_parcel_HTC = all_parcel_HTC.fillna(0)
 
-# percentage of areas in HCT tract area
-all_parcel_HTC['percent_area'] = all_parcel_HTC['HCT_Area'] / all_parcel_HTC['Shape_Area']
+# 4. percentage of areas in HCT tract area
+all_parcel_HTC['percent_area'] = all_parcel_HTC['Buffer_Area'] / all_parcel_HTC['Shape_Area']
 today = datetime.date.today()
-file_name = 'tract_area_2025_' + str(today) + '.csv'
+file_name = 'item9_proximity_area_2025_' + str(today) + '.csv'
 all_parcel_HTC.to_csv(os.path.join(output_path, file_name), index=False)
 
 # the region
-p = all_parcel_HTC['HCT_Area'] .sum()/all_parcel_HTC['Shape_Area'].sum()
+p = all_parcel_HTC['Buffer_Area'] .sum()/all_parcel_HTC['Shape_Area'].sum()
 print p 
